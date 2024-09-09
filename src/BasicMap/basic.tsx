9e5610d5c1df24470330c954deac1b02ad44db70
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-09-09 05:07:05 +0100

    install sass and implement a basic map

---
+++ src/BasicMap/basic.tsx
@@ -0,0 +1,35 @@
+
+import {useEffect,useRef, useState} from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { BasicMapProps } from "./interface";
+import "./style.scss"
+
+export default function BasicMap({longitude,latitude,zoom,accessToken}:BasicMapProps){
+    const [Longitude, setLongitude] = useState(longitude);
+    const [Latitude, setLatitude] = useState(latitude);
+    const [Zoom, setZoom] = useState(zoom);
+    const mapContainerRef = useRef<HTMLDivElement>(null);
+    useEffect(()=>{
+        mapboxgl.accessToken = accessToken
+        if (mapContainerRef.current !== null) {
+                new mapboxgl.Map({
+                    container:mapContainerRef.current,
+                    center:[Longitude,Latitude],
+                    zoom:Zoom
+                })
+        }
+
+    });
+
+    useEffect(()=>{
+        setLatitude(()=>latitude);
+        setLongitude(()=>longitude);
+        setZoom(()=>Zoom)
+    },[longitude,latitude,zoom,accessToken])
+
+    return (
+                <div id="mapContainer" ref={mapContainerRef}>
+                </div>
+            )
+}
---
+++ src/BasicMap/interface.ts
@@ -0,0 +1,8 @@
+
+
+export interface BasicMapProps{
+    longitude:number,
+    latitude:number,
+    zoom:number,
+    accessToken:string
+}
