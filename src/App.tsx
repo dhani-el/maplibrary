12230d0252a4c81fa2011d55768b09b7d0310a2b
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-09-09 09:57:00 +0100

    implement a basic map with no extra features

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 
 import BasicMap from "./BasicMap/basic"
-
+import MarkerlessMap from "./MarkerLessMap/markerless"
 function App() {
 const token = import.meta.env.VITE_MAP_BOX_ACCESS_TOKEN
 
 
   return (
     <div style={{height:"100vh"}}>
-      <BasicMap  longitude={3.3515} latitude={6.6018} accessToken={token} zoom={10}/>
+      <MarkerlessMap  longitude={3.3515} latitude={6.6018} accessToken={token} zoom={10}/>
     </div>
   )
 }
